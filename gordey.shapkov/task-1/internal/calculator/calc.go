66e3c92b1c544fde017358247e9f108958f103d4
TheEmfield <160738966+TheEmfield@users.noreply.github.com>
2025-09-24 00:38:15 +0300

    [TASK-1]  (#11)
    * [TASK-1] small fix
    
    * [TASK-1] removed else in calc.go

---
+++ gordey.shapkov/task-1/internal/calculator/calc.go
@@ -0,0 +1,26 @@
+package calculator
+
+import "errors"
+
+var (
+	errDivisionByZero   = errors.New("Division by zero")
+	errInvalidOperation = errors.New("Invalid operation")
+)
+
+func Calculation(a, b int, op string) (int, error) {
+	switch op {
+	case "+":
+		return a + b, nil
+	case "-":
+		return a - b, nil
+	case "*":
+		return a * b, nil
+	case "/":
+		if b == 0 {
+			return 0, errDivisionByZero
+		}
+		return a / b, nil
+	default:
+		return 0, errInvalidOperation
+	}
+}
