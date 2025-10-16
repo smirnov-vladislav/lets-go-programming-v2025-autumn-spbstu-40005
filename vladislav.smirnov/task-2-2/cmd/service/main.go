650716b1e0fb5857ed6e035fc2b7ef0a893257bc
smirnov-vladislav <smirnovvladislav505@gmail.com>
2025-10-16 18:16:24 +0300

    [TASK-2-2] initial commit

---
+++ vladislav.smirnov/task-2-2/cmd/service/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+  "fmt"
+  "container/heap"
+
+  "github.com/smirnov-vladislav/task-2-2/internal/intheap"
+)
+
+func main() {
+  var dishesCount int
+
+  if _, err := fmt.Scan(&dishesCount); err != nil {
+    fmt.Printf("error scanning count of dishes: %v\n", err)
+
+    return
+  }
+
+  dishes := &intheap.IntHeap{}
+  heap.Init(dishes)
+
+  for range dishesCount {
+    var value int
+    if _, err := fmt.Scan(&value); err != nil {
+      fmt.Printf("error scanning value of dish: %v\n", err)
+
+      return
+    }
+
+    heap.Push(dishes, value)
+  }
+
+  var wished int
+  if _, err := fmt.Scan(&wished); err != nil {
+    fmt.Printf("error scanning wished dish: %v\n", err)
+
+    return
+  }
+
+  for range wished - 1 {
+    if heap.Pop(dishes) == nil {
+      fmt.Printf("error: heap is empty")
+
+      return
+    }
+  }
+
+  bestDish := heap.Pop(dishes)
+  if bestDish == nil {
+    fmt.Printf("error: heap is empty")
+
+    return
+  }
+
+  fmt.Println(bestDish)
+}
