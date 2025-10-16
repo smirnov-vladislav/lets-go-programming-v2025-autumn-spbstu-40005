a35d35e2168c32d874a543d4d13534d54e90eb6b
smirnov-vladislav <smirnovvladislav505@gmail.com>
2025-10-17 02:12:43 +0300

    [TASK-1]  (#34)
    * [TASK-1] initial commit for task-1
    
    * [TASK-1] fix formatting issues

---
+++ vladislav.smirnov/task-1/cmd/service/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var (
+		oper1, oper2 int
+		operation    string
+	)
+
+	_, err := fmt.Scan(&oper1)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+	_, err = fmt.Scan(&oper2)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+	_, err = fmt.Scan(&operation)
+	if err != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
+
+	switch operation {
+	case "+":
+		fmt.Println(oper1 + oper2)
+	case "-":
+		fmt.Println(oper1 - oper2)
+	case "*":
+		fmt.Println(oper1 * oper2)
+	case "/":
+		if oper2 == 0 {
+			fmt.Println("Division by zero")
+			return
+		}
+		fmt.Println(oper1 / oper2)
+	default:
+		fmt.Println("Invalid operation")
+	}
+}
