9c8f991eacb6666eb14840564fc6ab75963b4668
smirnov-vladislav <smirnovvladislav505@gmail.com>
2025-10-28 23:51:15 +0300

    [TASK-2-1]  (#73)
    * [TASK-2-1] initial commit
    
    * [TASK-2-1] fix linter
    
    * [TASK-2-1] fix formatting problems
    
    * [TASK-2-1] change go.mod
    
    * [TASK-2-1] change print of errors
    
    * [TASK-2-1] made function applying temperature local
    
    * [TASK-2-1] little fix

---
+++ vladislav.smirnov/task-2-1/cmd/service/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+type TemperatureProcessor struct {
+	upper int
+	lower int
+}
+
+const (
+	UpperTemp = 30
+	LowerTemp = 15
+)
+
+var (
+	errOperator = errors.New("incorrect operator")
+	errNotExist = errors.New("optimal temperature does not exist")
+)
+
+func NewTemperatureProcessor() *TemperatureProcessor {
+	return &TemperatureProcessor{
+		upper: UpperTemp,
+		lower: LowerTemp,
+	}
+}
+
+func (tp *TemperatureProcessor) apply(oper string, value int) error {
+	switch oper {
+	case "<=":
+		if value < tp.upper {
+			tp.upper = value
+		}
+	case ">=":
+		if value > tp.lower {
+			tp.lower = value
+		}
+	default:
+		return errOperator
+	}
+
+	return nil
+}
+
+func (tp *TemperatureProcessor) getOptimalTemp() (int, error) {
+	if tp.upper < tp.lower {
+		return 0, errNotExist
+	}
+
+	return tp.lower, nil
+}
+
+func main() {
+	var departmentCount int
+	if _, err := fmt.Scan(&departmentCount); err != nil {
+		fmt.Println("error reading department count: ", err)
+
+		return
+	}
+
+	for range departmentCount {
+		processor := NewTemperatureProcessor()
+
+		var employeeCount int
+		if _, err := fmt.Scan(&employeeCount); err != nil {
+			fmt.Println("error reading employee count: ", err)
+
+			return
+		}
+
+		for range employeeCount {
+			var (
+				oper  string
+				value int
+			)
+
+			if _, err := fmt.Scan(&oper, &value); err != nil {
+				fmt.Println("error reading new temperature: ", err)
+
+				return
+			}
+
+			if err := processor.apply(oper, value); err != nil {
+				fmt.Println("error applying operation: ", err)
+
+				return
+			}
+
+			if res, err := processor.getOptimalTemp(); err != nil {
+				fmt.Println(-1)
+			} else {
+				fmt.Println(res)
+			}
+		}
+	}
+}
